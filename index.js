6106d8597e0d225e923b63e2fff76795a348be89
user name <you@example.com>
2024-06-27 06:53:36 +0300

    sec commit

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { userRouter } from "./modules/users/users.routes.js";
 import postsRouter from "./modules/posts/posts.routes.js";
+import cors from "cors";
 const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.use("/auth", userRouter);
 app.use("/posts", postsRouter);
 app.listen(port, () => console.log(`server is running on port ${port}`));
